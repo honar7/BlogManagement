0b3433cdb0c97837a93426bb05080b4af31139d7
Kourosh <honar7@gmail.com>
2021-06-17 08:31:58 +0430

    add user entity

---
+++ BlogManagement.Core.Domain/Users/User.cs
@@ -0,0 +1,18 @@
+﻿using Golrang.Framework.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogManagement.Core.Domain.Users
+{
+     
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
